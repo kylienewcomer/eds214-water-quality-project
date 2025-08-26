f4891632774d68c27c52ddcf832ef87c30534319
kylienewcomer <kylienewcomer@ucsb.edu>
2025-08-26 14:56:16 -0700

    read me

--- data-cleaning.R
+++ data-cleaning.R
@@ -3,6 +3,7 @@ library(here)
 library(janitor)
 library(forecast)
 library(zoo)
+library(lubridate)
 
 rm(list = ls())
 
@@ -32,11 +33,9 @@ clean_conc <- all %>%
 conc_long <- clean_conc %>% 
     pivot_longer("no3_n":"k", names_to = "nutrient",
                  values_to = "concentration") %>% 
-  mutate(days_before = no_day - 22,
-         days_after = no_day +22)
+  mutate(days_before = no_day - 31,
+         days_after = no_day +31)
 
-if days_before >= 31
-  & days_after <=
 
 
 conc_av <- conc_long %>% 
@@ -57,17 +56,43 @@ week_class <- function(day, n = n){
       weeks_after <- (day + 22)
   }
 }
+conc_long %>% mutate(
+  start_date = sample_date - day(31),
+       end_date = sample_date + day(31),
+       average = seq(start_date, end_date),
+average_invt = int_diff(average))
+
+mean(sample_date[i] %within% average_invt)
 
 
-TTR::SMA(conc_long, n = 31)
 
 means <- function(concentration){rollmean(concentration, 31)}
 
 means(conc_av, concetration = concentration, k = no_day)
 
 
-sapply(c("right","center","left"),
-       function(x)zoo::rollmean(test,7,align = x, na.pad = TRUE))
+
+range <- day(1)
+
+conc <- conc_long %>%
+  group_by(sample_id, nutrient, sample_date) %>%
+  mutate(interval(sample_date - range, sample_date + range))
+    
+    Npoints = 1:n() - interval(sample_date - range, sample_date + range)),
+         mean = rollapplyr(concentration, width = 9, 
+                           FUN = mean(concentration), partial = TRUE, 
+                           fill = NA)) %>%
+  ungroup
+
+w <- 3 
+data %>%
+  group_by(Group) %>%
+  mutate(Npoints = 1:n() - interval(Date - w, Date + )),
+         Mean3 = rollapplyr(Value, Npoints, mean, partial = TRUE, fill = NA)) %>%
+  ungroup
+
+#sapply(c("right","center","left"),
+       #function(x)zoo::rollmean(test,7,align = x, na.pad = TRUE))
 
 
 #week_class(day = 308)
