0d19a1592de0ea3ade48818f9d5f80a17b503175
kylienewcomer <kylienewcomer@ucsb.edu>
2025-08-27 12:03:36 -0700

    conflict

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # eds214-water-quality-project
 
-# 1 Data
-Data is from McDowell, W. and International Institute of Tropical Forestry(IITF), USDA Forest Service.. 2024. Chemistry of stream water from the Luquillo Mountains ver 4923064. Environmental Data Initiative. https://doi.org/10.6073/pasta/f31349bebdc304f758718f4798d25458 (Accessed 2025-08-26).
+## 1 Data
+
+Data is from McDowell, W. and International Institute of Tropical Forestry(IITF), USDA Forest Service.. 2024. Chemistry of stream water from the Luquillo Mountains ver 4923064. Environmental Data Initiative. <https://doi.org/10.6073/pasta/f31349bebdc304f758718f4798d25458> (Accessed 2025-08-26).
+
+## 2 Clean
--- data-cleaning.R
+++ data-cleaning.R
@@ -1,11 +1,17 @@
+rm(list = ls())
+
+# Loading in packages
 library(tidyverse)
 library(here)
 library(janitor)
 library(forecast)
 library(zoo)
 library(lubridate)
+library(slider)
 
-rm(list = ls())
+
+
+# Reading in data
 
 bq1 <- read_csv(here("data", "knb-lter-luq.20.4923064", "QuebradaCuenca1-Bisley.csv"))
 
@@ -15,47 +21,56 @@ bq3 <- read_csv(here("data", "knb-lter-luq.20.4923064", "QuebradaCuenca2-Bisley.
 
 prm <- read_csv(here("data", "knb-lter-luq.20.4923064", "RioMameyesPuenteRoto.csv"))
 
+# Combining data
 bq_12 <- rbind(bq1, bq2)
 
 bq <- rbind(bq_12, bq3)
 
 all <- rbind(bq, prm)
 
+#cleaning
+
 clean_conc <- all %>% 
+  
   clean_names() %>% 
+  
   select(sample_id, sample_date, no3_n, nh4_n, mg, ca, k) %>% 
+  
   mutate(day_of_year = lubridate::yday(sample_date)) %>% 
+
+  
   separate(col = sample_date, 
            into = c("year", "month", "day"), sep = "-", remove = FALSE) %>% 
+  
+  filter(year <= 1995) %>% 
+  
+  #want to add a column of day relative to every day in 
   mutate(no_day = (as.numeric(year)-1986)*365 + day_of_year)
 
 
 conc_long <- clean_conc %>% 
+  
+  #changing data format to better use in ggplot later
     pivot_longer("no3_n":"k", names_to = "nutrient",
                  values_to = "concentration") %>% 
+  
+  #adding rows to classify for 9 week rolling mean
   mutate(days_before = no_day - 31,
          days_after = no_day +31)
 
 
-
+#practicing means
 conc_av <- conc_long %>% 
   group_by(sample_id, nutrient, year) %>% 
   summarize(ave = mean(concentration, na.rm = TRUE))
 
-
+#plotting mean
 ggplot(conc_av, aes(year, ave)) +
   geom_point(aes(color = sample_id)) +
   facet_wrap(~nutrient, scales = "free")
 
 
 
-week_class <- function(day, n = n){
-  for (i in nrow(conc_long)){
-    group_by(group_id, day) %>% 
-      weeks_before <- (day - 22)
-      weeks_after <- (day + 22)
-  }
-}
 conc_long %>% mutate(
   start_date = sample_date - day(31),
        end_date = sample_date + day(31),
@@ -73,27 +88,56 @@ means(conc_av, concetration = concentration, k = no_day)
 
 
 range <- day(1)
+# trash
 
-conc <- conc_long %>%
-  group_by(sample_id, nutrient, sample_date) %>%
-  mutate(interval(sample_date - range, sample_date + range))
+#conc <- conc_long %>%
+  #group_by(sample_id, nutrient, sample_date, no_day) %>%
+  
+ # mutate(interval(sample_date - range, sample_date + range))
     
-    Npoints = 1:n() - interval(sample_date - range, sample_date + range)),
-         mean = rollapplyr(concentration, width = 9, 
-                           FUN = mean(concentration), partial = TRUE, 
-                           fill = NA)) %>%
+   # Npoints = 1:n() - interval(sample_date - range, sample_date + range)),
+        # mean = rollapplyr(concentration, width = 9, 
+                          # FUN = mean(concentration), partial = TRUE, 
+                           #fill = NA) %>%
+ # ungroup
+         
+conc_subset <- conc_long %>% 
+  slice_head(n = 40)
+
+
+conc <- conc_subset %>%
+  group_by(sample_id, nutrient, sample_date, no_day, concentration) %>% 
+  mutate(rolling_conc = rollmean(concentration, 31))
+
+
+  
+  mutate(Npoints = 1:n() - interval(Date - w, Date + )),
+         Mean3 = rollapplyr(Value, Npoints, mean, 
+                            partial = TRUE, fill = NA)) %>%
+  
   ungroup
 
-w <- 3 
-data %>%
-  group_by(Group) %>%
-  mutate(Npoints = 1:n() - interval(Date - w, Date + )),
-         Mean3 = rollapplyr(Value, Npoints, mean, partial = TRUE, fill = NA)) %>%
-  ungroup
+conc_mean <- conc_subset %>%
+  group_by(sample_id, nutrient) %>%
+  mutate(roll_mean = slide_index_dbl(concentration, sample_date, 
+                                     .f = mean,
+                                      before = 31L, after = 31L, 
+                                      complete = FALSE, na_rm = TRUE))
 
 #sapply(c("right","center","left"),
        #function(x)zoo::rollmean(test,7,align = x, na.pad = TRUE))
+for (i in seq_along(sample_date)){
+  group_by()
+  if(no_day >= days_before | no_day <= days_after){
+    mean = mean
+  }
+  
+  
+}
 
-
+ggplot(conc_mean, aes(sample_date, roll_mean)) +
+  geom_point(aes(color = sample_id)) +
+  facet_wrap(~nutrient, scales = "free")
+  
 #week_class(day = 308)
 
